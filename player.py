26aa89efe11212c8b8b5d566d705d71a5e8c761b
Matt Redmond <matt.redmond@katalyst.com.au>
2025-01-19 16:08:26 +1030

    Add player shooting

--- player.py
+++ player.py
@@ -3,12 +3,14 @@ from pygame import Vector2
 
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
 
     def triangle(self):
         forward = Vector2(0, 1).rotate(self.rotation)
@@ -28,8 +30,14 @@ class Player(CircleShape):
         forward = Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.cooldown = PLAYER_SHOOT_COOLDOWN
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
@@ -39,3 +47,6 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE]:
+            if self.cooldown <= 0:
+                self.shoot()
